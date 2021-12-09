2831458d7aa0f2fb5d948045e3a4b2541cb16eec
walid-peach <walidelkhoukh99@gmail.com>
2021-12-09 23:20:18 +0100

    first step to plot the histogram

---
+++ main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime 
+
+header = st.container()
+
+with header:
+    st.title("Welcome to Statistical Process Control application")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+#data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+#data_load_state.text('Loading data...done!')
+
+st.subheader('Raw data')
+#st.write(data)
+
+#st.subheader('Number of pickups by hour')
+#hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+#st.bar_chart(hist_values)
+
+@st.cache(allow_output_mutation=True)
+def get_data():
+    return []
+
+user_id = st.text_input("User ID")
+
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("Add row"):
+        get_data().append(user_id)
+
+with col2:
+    if st.button("clear last row"):
+        get_data().pop()
+
+st.subheader('Number of pickups by hour')
+a=[1,2,1,2,1,2]
+hist_values =np.histogram(np.array(get_data()).astype(np.float),bins=1)
+st.bar_chart(hist_values)
+st.write(np.array(get_data()).astype(np.float))
+
