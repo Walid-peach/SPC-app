39b1bb59d5ce600d16dd337c47e9e30277d7407a
walid-peach <walidelkhoukh99@gmail.com>
2021-12-14 17:46:58 +0100

    dynamic navbar

--- main.py
+++ main.py
@@ -31,27 +31,29 @@ def navigation():
         st.error('Please use the main app.')
         return None
     return menu_id
+@st.cache(allow_output_mutation=True)
+def get_data():
+    return []
 
 if st.sidebar.button('click me too'):
   st.info('You clicked at: {}'.format(datetime.datetime.now()))
 
 
+
+#RUN
+menu_id=navigation()
 header = st.container()
-
 with header:
+    st.title("Welcome to Statistical Process Control application")
 
-    if navigation() == "Home":
+if menu_id == "Home":
         st.title('Home')
         st.info('This is the home page.')     
-        st.title("Welcome to Statistical Process Control application")
+        
     
-    elif navigation()=="Insert Data manually":
-        st.title("Welcome to Statistical Process Control application")
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        data_load_state = st.text('Enter data please!')
-
-
-
+elif menu_id =="Insert Data manually":
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    data_load_state = st.text('Enter data please!')
 
 # Create a text element and let the reader know the data is loading.
 #data_load_state = st.text('Enter data please!')
@@ -59,45 +61,38 @@ with header:
 #data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 #data_load_state.text('Loading data...done!')
-
 #st.subheader('Raw data')
-
-
 #disable this warning by disabling the config option: deprecation.showPyplotGlobalUse
 #st.set_option('deprecation.showPyplotGlobalUse', False)
+    user_id = st.text_input("User ID")
 
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button("Add row"):
+            get_data().append(user_id)
 
-@st.cache(allow_output_mutation=True)
-def get_data():
-    return []
+    with col2:
+        if st.button("clear last row"):
+            get_data().pop()
+            
+            
+    # Test normality of data distribution
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111) # 111 is equivalent to nrows=1, ncols=1, plot_number=1.
+    prob = stats.probplot(np.array(get_data()).astype(np.float), dist=stats.norm, plot=ax1)
+    ax1.set_xlabel('')
+    ax1.set_title('Probability plot against normal distribution')
+    plt.show()
+    st.pyplot()
 
-user_id = st.text_input("User ID")
+    #---------------------------------------------------
+    #Plot the histogram
+    
 
-col1, col2 = st.columns(2)
-with col1:
-    if st.button("Add row"):
-        get_data().append(user_id)
-
-with col2:
-    if st.button("clear last row"):
-        get_data().pop()
-        
-        
-# Test normality of data distribution
-fig = plt.figure()
-ax1 = fig.add_subplot(111) # 111 is equivalent to nrows=1, ncols=1, plot_number=1.
-prob = stats.probplot(np.array(get_data()).astype(np.float), dist=stats.norm, plot=ax1)
-ax1.set_xlabel('')
-ax1.set_title('Probability plot against normal distribution')
-plt.show()
-st.pyplot()
-
-
-
-st.subheader('Number of pickups by hour')
-st.write(np.array(get_data()).astype(np.float))
-plt.hist(np.array(get_data()).astype(np.float))
-plt.show()
-st.pyplot()
+    st.subheader('Number of pickups by hour')
+    st.write(np.array(get_data()).astype(np.float))
+    plt.hist(np.array(get_data()).astype(np.float))
+    plt.show()
+    st.pyplot()
 
 
